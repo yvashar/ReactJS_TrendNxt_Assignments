8e2f15256150b49ed966b9c5deb0d64aede8b327
yvasahar <yvasahar@synamedia.com>
2022-01-18 21:48:39 +0530

    JSX-Assign2_added

---
+++ jsx_assign_2/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Employee from './Employee';
+function App() {
+  return (
+    <div className="App">
+      <header> 
+        <strong>
+          Employee Details 
+        </strong>
+      </header>
+      <Employee/>
+    </div>
+  );
+}
+
+export default App;
